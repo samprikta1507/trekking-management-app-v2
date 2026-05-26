e9d3cd6a059b586e7dbb667745e3300f0b73b47f
Samprikta Malik <21f3002038@ds.study.iitm.ac.in>
2026-05-26 14:53:23 +0530

    basic_set_up_of_flask_app

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///trekking.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
