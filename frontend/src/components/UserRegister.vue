637fec4bce26ce0fac6152323b0ef7cf3a6eac17
Samprikta Malik <21f3002038@ds.study.iitm.ac.in>
2026-05-30 21:50:40 +0530

    added_some_vue_files_and_added_basic_navigation_in_LandingView

---
+++ frontend/src/components/UserRegister.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>User Registration</h1>
+        <form>
+            
+        </form>
+
+    </div>
+
+
+</template>
+
+<script>
+import { d } from 'vue-router/dist/useApi-D6ckOsFy.js';
+
+
+</script>
---
+++ frontend/src/views/LandingView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>
+        <div @click="$router.push('/admin')">
+            Admin view 
+        </div>
+    </div>
+    <div>
+        <div @click="$router.push('/user')">
+            User view
+        </div>
+    </div>
+    <div>
+        <div @click="$router.push('/staff')">
+            Staff view
+        </div>
+    </div>
+
+  </div>
+</template> 
+
+<script>
+export default {
+  name: 'LandingView',
+}
+</script>
